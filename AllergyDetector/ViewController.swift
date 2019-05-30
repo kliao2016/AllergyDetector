5dc51bd3a5dd678e2112c8381bc1975f6e7c053b
Kevin Liao <kliao2016@gmail.com>
2019-05-30 05:37:32 -0700

    add snapkit

---
+++ AllergyDetector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllergyDetector
+//
+//  Created by Kevin Liao on 5/30/19.
+//  Copyright © 2019 Kevin Liao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
